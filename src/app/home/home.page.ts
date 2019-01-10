b0e3a1345500227cd64606e15a31c98640cc238e
Aaron S. Hawley <aaron.s.hawley@gmail.com>
2019-01-10 10:08:11 -0500

    Mock lesson model

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,4 +7,22 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
+  items: Array<any> = [
+    {
+      title: "Unit 10",
+      subtitle: "Substraction strategies",
+      items: [
+        {
+          title: "Lesson 10.1",
+          subtitle: "One Less, Ten Less",
+          date: "2019-05-28"
+        },
+        {
+          title: "Lesson 3.2",
+          subtitle: "Counting back form 10",
+          date: "2019-05-28"
+        }
+      ]
+    }
+  ]
 }
